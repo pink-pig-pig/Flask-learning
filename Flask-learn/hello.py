d5d0e34baa4b2c33d5383d176a1ab12f0ed50fed
张亚中 <46549169+pink-pig-pig@users.noreply.github.com>
2019-01-22 16:51:11 +0800

    Update hello.py
    添加了数据库flask-sqlalchemy
    添加欢迎信息针对于新老用户

--- Flask-learn/hello.py
+++ Flask-learn/hello.py
@@ -5,28 +5,64 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Hard to guess string!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 bootstrap = Bootstrap(app)
 moment    = Moment(app)
+db        = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id    = db.Column(db.Integer, primary_key=True)
+    name  = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>'%(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id       = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    role_id  = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<user %r>'%(self.username)
+
 
 class NameForm(FlaskForm):
     name   = StringField('what is your name', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def formtest():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have change your name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+
         session['name'] = form.name.data
 
         return redirect(url_for('formtest'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
 @app.route('/user/<name>')
 def index(name):
     return render_template('user.html', name=name)
