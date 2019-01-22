cc289ba69f0df6383f053516c716b1e82d813345
张亚中 <46549169+pink-pig-pig@users.noreply.github.com>
2019-01-22 20:16:50 +0800

    Update hello.py
    集成了电子邮件发送功能！

--- Flask-learn/hello.py
+++ Flask-learn/hello.py
@@ -7,7 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_mail import Mail
+from flask_mail import Mail, Message
 import os
 
 
@@ -22,6 +22,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 bootstrap = Bootstrap(app)
 moment    = Moment(app)
@@ -63,10 +66,13 @@ def formtest():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
 
         session['name'] = form.name.data
+        form.name.data = ''
 
         return redirect(url_for('formtest'))
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
@@ -91,4 +97,13 @@ def internal_server_error(e):
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, mail=mail, Message=Message)
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+
+    mail.send(msg)
+
